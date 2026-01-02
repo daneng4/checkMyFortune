eba2cca9fd5150f64602bf1f78e79d46e46be160
daneng4 <daneng4@naver.com>
2026-01-02 17:54:33 +0900

    [CMF] first commit
    description : first commit for setting project

---
+++ src/main/java/com/eskim/checkmyfortune/CheckMyFortuneApplication.java
@@ -0,0 +1,13 @@
+package com.eskim.checkmyfortune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckMyFortuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckMyFortuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eskim/checkmyfortune/CheckMyFortuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eskim.checkmyfortune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckMyFortuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
